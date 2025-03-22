99ea344edc5a3c6b07edd3365d00df5be148ea8b
shopifydev-ig <shopifydev@gmail.com>
2025-03-22 23:12:03 +0200

    add more IvoCoin tests

--- test/IvoCoin.t.sol
+++ test/IvoCoin.t.sol
@@ -6,18 +6,42 @@ import "../src/IvoCoin.sol";
 
 contract IvoCoinTest is Test {
     IvoCoin token;
+    uint constant INITIAL_SUPPLY = 1000;
 
     function setUp() public {
-        token = new IvoCoin(1000); // 1000 tokens minted to deployer
+        token = new IvoCoin(INITIAL_SUPPLY); // 1000 tokens minted to deployer
     }
 
-    function testInitialSupply() public {
-        assertEq(token.totalSupply(), 1000 * 10 ** token.decimals());
+    function testInitialSupply() public view {
+        assertEq(token.totalSupply(), INITIAL_SUPPLY * (10 ** token.decimals()));
     }
 
     function testTransfer() public {
         address recipient = address(0x123);
-        token.transfer(recipient, 100 * 10 ** token.decimals());
-        assertEq(token.balanceOf(recipient), 100 * 10 ** token.decimals());
+        token.transfer(recipient, 100 * (10 ** token.decimals()));
+        assertEq(token.balanceOf(recipient), 100 * (10 ** token.decimals()));
+    }
+
+    function testMint() public {
+        address recipient = address(0x123);
+        token.mint(recipient, 100 * (10 ** token.decimals()));
+        assertEq(token.balanceOf(recipient), 100 * (10 ** token.decimals()));
+    }
+
+    function testBurn() public {
+        token.burn(INITIAL_SUPPLY * (10 ** token.decimals()));
+        assertEq(token.balanceOf(address(this)), 0);
+    }
+
+    function testSetOwner() public {
+        address newOwner = address(0x123);
+        token.setOwner(newOwner);
+        assertEq(token.owner(), newOwner);
+    }
+
+    function testSetMinter() public {
+        address newMinter = address(0x123);
+        token.setMinter(newMinter);
+        assert(token.hasRole(token.MINTER_ROLE(), newMinter));
     }
 }
